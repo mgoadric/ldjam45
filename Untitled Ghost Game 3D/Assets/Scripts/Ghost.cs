1bf6c41faa43afed612450f3226644520e976a57
Mark Goadrich <goadrich@hendrix.edu>
2019-10-04 23:38:51 -0500

    Ghost is floating! but runs into walls...

--- Untitled Ghost Game 3D/Assets/Scripts/Ghost.cs
+++ Untitled Ghost Game 3D/Assets/Scripts/Ghost.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // https://answers.unity.com/questions/1373810/how-to-move-the-character-using-wasd.html
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.forward);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.back);
+
     }
 }
