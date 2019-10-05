c196dcf673f1923486a39beb9ef093181e13d214
samoreGE <samoreGE@hendrix.edu>
2019-10-05 13:30:15 -0500

    Seperate Power script, Push CD implemented

--- Untitled Ghost Game 3D/Assets/Scripts/Ghost.cs
+++ Untitled Ghost Game 3D/Assets/Scripts/Ghost.cs
@@ -7,17 +7,13 @@ public class Ghost : MonoBehaviour
 
     private Rigidbody rb;
     public float moveSpeed;
-    public float pushStrength;
-    public float pushRadius;
-    private bool hasPushed;
-    public GameObject pushEffect;
+    public Powers powers;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        hasPushed = false;
     }
 
     // Update is called once per frame
@@ -34,38 +30,11 @@ public class Ghost : MonoBehaviour
             rb.AddForce(Vector3.back * moveSpeed);
         if (Input.GetKey(KeyCode.Space))
         {
-            Push(pushRadius);
+            powers.SpacePressed();
         } else
         {
-            hasPushed = false;
+            powers.NoInput();
         }
 
-    }
-
-    private void Push(float radius)
-    {        
-        if(!hasPushed)
-        {
-            Instantiate(pushEffect, transform.position, Quaternion.identity);
-            Debug.Log(message: "Pushing, radius=" + radius);
-            Collider[] hitColliders = Physics.OverlapSphere(rb.position, radius);
-            int i = 0;
-            string objectForces = "";
-            while (i < hitColliders.Length)
-            {
-                GameObject hitObject = hitColliders[i].gameObject;
-                Vector3 heading = hitObject.transform.position - rb.position;
-                var distance = heading.magnitude;
-                if (hitObject.tag == "Moveable")
-                {
-                    Vector3 pushForce = (heading / distance * (1 / distance) * pushStrength);
-                    hitObject.GetComponent<Rigidbody>().AddForce(pushForce);
-                    objectForces += " hit " + hitObject.name + " with force " + pushForce.magnitude;
-                }
-                i++;
-            }
-            Debug.Log(message: objectForces);
-            hasPushed = true;
-        }
     }
 }
---
+++ Untitled Ghost Game 3D/Assets/Scripts/Powers.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powers : MonoBehaviour
+{
+    private Transform tf;
+
+    public float hauntRadius;
+
+    public float pushStrength;
+    public float pushRadius;
+    public float pushCD;
+    private float nextPush;
+    private bool canPush;
+
+
+    public GameObject pushEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        canPush = true;
+        nextPush =0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NoInput()
+    {
+        if(Time.time > nextPush)
+        {
+            canPush = true;
+        }
+        
+    }
+
+    public void NoPush()
+    {
+        canPush = true;
+    }
+
+    public void SpacePressed()
+    {
+        if(canPush)
+        {
+            Push();
+        }
+    }
+
+    public void Push()
+    {    
+        Instantiate(pushEffect, transform.position, Quaternion.identity);
+        Debug.Log(message: "Pushing, radius=" + pushRadius);
+        Collider[] hitColliders = Physics.OverlapSphere(tf.position, pushRadius);
+        int i = 0;
+        string objectForces = "";
+        while (i < hitColliders.Length)
+        {
+            GameObject hitObject = hitColliders[i].gameObject;
+            Vector3 heading = hitObject.transform.position - tf.position;
+            var distance = heading.magnitude;
+            if (hitObject.tag == "Moveable")
+            {
+                Vector3 pushForce = (heading / distance * (1 / distance) * pushStrength);
+                hitObject.GetComponent<Rigidbody>().AddForce(pushForce);
+                objectForces += " hit " + hitObject.name + " with force " + pushForce.magnitude;
+            }
+            i++;
+        }
+        Debug.Log(message: objectForces);
+        nextPush = Time.time + pushCD;
+        canPush = false;
+    }
+}
