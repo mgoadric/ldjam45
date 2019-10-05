e2c004159cd06c01463c01f7fb60886fd04a6973
Mark Goadrich <goadrich@hendrix.edu>
2019-10-04 22:32:37 -0500

    Getting the initial objects and scripts created to manage the game

---
+++ Untitled Ghost Game 3D/Assets/Scripts/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State { START, MIDDLE, END }
+
+public class Manager : MonoBehaviour
+{
+
+    public GameObject gamePrefab;
+    public State gameState;
+    public GameObject game;
+
+    public GameObject dialogbox;
+    public GameObject instructions;
+
+    public static Manager S;
+
+    private void Awake()
+    {
+        S = this;
+        Debug.Log("Starting");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        if (game)
+        {
+            Destroy(game);
+        }
+        game = Instantiate(gamePrefab);
+        StartCoroutine("GameScript");
+
+    }
+
+    IEnumerator GameScript()
+    {
+
+        // SETUP
+
+        // WAIT FOR PLAYER TO DO SOMETHING
+
+        while (gameState == State.START)
+        {
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+}
