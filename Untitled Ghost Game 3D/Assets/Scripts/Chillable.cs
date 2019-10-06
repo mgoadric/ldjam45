4941979733e4c40eb5a452bbd989154921e11847
Mark Goadrich <goadrich@hendrix.edu>
2019-10-05 23:12:05 -0500

    Starting work on the chilling mechanic

---
+++ Untitled Ghost Game 3D/Assets/Scripts/Chillable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chillable : MonoBehaviour
+{
+
+    private float stayCount = 0.0f;
+    public bool chilled;
+    public bool whispered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        stayCount = 0;
+    }
+
+    // stayCount allows the OnTriggerStay to be displayed less often
+    // than it actually occurs.
+    private void OnTriggerStay(Collider other)
+    {
+        stayCount = stayCount + Time.deltaTime;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!chilled)
+        {
+            chilled |= stayCount > 3.0f;
+        } else if (!whispered)
+        {
+            whispered |= stayCount > 5.0f;
+        }
+    }
+}
--- Untitled Ghost Game 3D/Assets/Scripts/Resident.cs
+++ Untitled Ghost Game 3D/Assets/Scripts/Resident.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,12 +10,42 @@ public class Resident : MonoBehaviour
     public Transform goal2;
     private NavMeshAgent agent;
 
+    public GameObject dialogBubble;
+
+    private bool talking;
+
+    public void Dialog(string line, float duration)
+    {
+        if (!talking)
+        {
+            talking = true;
+
+            // make the resident dialog bubble appear
+            GameObject bar = Instantiate(dialogBubble, this.transform);
+
+            // add the text line for duration
+            GameObject go = bar.transform.GetChild(0).gameObject;
+            TextMeshPro tmpugui = go.GetComponent<TextMeshPro>();
+            tmpugui.SetText(line);
+
+            // make it disappear 
+            StartCoroutine(RemoveDialog(bar, duration));
+        }
+    }
+
+    IEnumerator RemoveDialog(GameObject go, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        talking = false;
+        Destroy(go);
+    }
 
     // Start is called before the first frame update
     void Start()
     {   
         agent = GetComponent<NavMeshAgent>();
         agent.destination = goal1.position;
+        talking = false;
     }
 
     // Update is called once per frame
