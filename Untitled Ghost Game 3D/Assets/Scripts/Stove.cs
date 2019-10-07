ae0d65958666b0769649ef34fc22595d76f7ccf5
samoreGE <samoreGE@hendrix.edu>
2019-10-07 00:01:18 -0500

    making things work

--- Untitled Ghost Game 3D/Assets/Scripts/Stove.cs
+++ Untitled Ghost Game 3D/Assets/Scripts/Stove.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Stove : MonoBehaviour
 {
+    public GameObject fireEffect;
+    public float xOffset, yOffset, zOffset;
+    private Transform tf;
+    private bool flameOn=false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tf = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPush()
     {
+        if(!flameOn)
+        {
+            Vector3 effectSource = tf.position;
+            fireEffect = Instantiate(fireEffect, effectSource, Quaternion.identity);
+            flameOn = true;
+        }
         
+    }
+    public void flameOff()
+    {
+        Destroy(fireEffect);
     }
 }
